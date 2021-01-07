bd09bf65d00e827cf18457fa10abe892ecde5719
Marso <marso.yu777@gmail.com>
2021-01-07 16:53:31 +0800

    NumCoverter Bug Fix

--- NumberConverter.swift
+++ NumberConverter.swift
@@ -8,9 +8,21 @@
 import Foundation
 func NumCoverter(_ number: Int) -> String {
     if number > 1000000{
-        return "\(number / 1000000),\((number % 1000000)/1000),\(number % 1000)"
+        let millionNumber = number / 1000000
+
+        let thousandNumber = String(format:"%03d",(number % 1000000) / 1000)
+
+        let lessThanThousnadNumber = String(format:"%03d",number % 1000)
+        
+        return "\(millionNumber),\(thousandNumber),\(lessThanThousnadNumber)"
+
     }else if number > 1000{
-        return "\(number / 1000),\(number % 1000)"
+        let thousandNumber = number / 1000
+        
+        let lessThanThousnadNumber = String(format:"%03d",number % 1000)
+        
+        return "\(thousandNumber),\(lessThanThousnadNumber)"
+ 
     }else{
     return String(number)
     }
